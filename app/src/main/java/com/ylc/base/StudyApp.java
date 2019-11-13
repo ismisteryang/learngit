178348cd494cead745a1c6b03094dd33e13dc2b4
yanglc <yanglc@bjttsf.com>
2019-11-13 15:12:29 +0800

    first commit

---
+++ app/src/main/java/com/ylc/base/StudyApp.java
@@ -0,0 +1,21 @@
+package com.ylc.base;
+
+import android.app.Application;
+
+/**
+ * ************************************************
+ * PROJECT_NAME : My Application
+ * PACKAGE_NAME : com.ylc.base
+ * AUTHOR : yanglinchuan
+ * DATA : 2019/9/26
+ * TIME : 19:31
+ * MSG :
+ * ************************************************
+ */
+public class StudyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
