686d61b6634428201a273a162fe1a4b42229e208
yanglc <yanglc@bjttsf.com>
2019-11-14 16:25:41 +0800

    新增的类

--- app/src/main/java/com/ylc/study/MasterCls.java
+++ app/src/main/java/com/ylc/study/MasterCls.java
@@ -13,6 +13,7 @@ package com.ylc.study;
 public class MasterCls {
     private String name;
     private String id;
+    private String text;
     public String getName() {
         return name;
     }
