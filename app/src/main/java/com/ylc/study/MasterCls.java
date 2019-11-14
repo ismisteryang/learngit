78b802b58c85288e434dc785a451287dee385b4b
yanglc <yanglc@bjttsf.com>
2019-11-14 11:43:50 +0800

    新增的类

---
+++ app/src/main/java/com/ylc/study/MasterCls.java
@@ -0,0 +1,14 @@
+package com.ylc.study;
+
+/**
+ * ************************************************
+ * PROJECT_NAME : My Application
+ * PACKAGE_NAME : com.ylc.study
+ * AUTHOR : yanglinchuan
+ * DATA : 2019/11/14
+ * TIME : 11:43
+ * MSG :
+ * ************************************************
+ */
+public class MasterCls {
+}
